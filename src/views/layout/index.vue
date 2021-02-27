3e569d07a51cdd790f9a6be794fe39091895bbc3
jyz-xx <215774124@qq.com>
2021-02-27 20:29:03 +0800

    1222

--- src/views/layout/index.vue
+++ src/views/layout/index.vue
@@ -1,39 +1,41 @@
 <template>
   <div class="layout-container">
     <!-- 子路由出口 -->
-    <router-view />
-    <!-- /子路由 -->
+    <router-view></router-view>
     <!-- 标签导航栏 -->
-    <!-- route 开启路由模式 -->
-    <van-tabbar class="layout-tabbar" route>
-      <van-tabbar-item to="/">
+    <van-tabbar v-model="active" class="layout-tabbar" route>
+      <van-tabbar-item icon="home-o" to="/">
         <i slot="icon" class="iconfont iconshouye"></i>
         <span class="text">首页</span>
       </van-tabbar-item>
-      <van-tabbar-item  to="/qa">
+      <van-tabbar-item icon="home-o" to="/qa">
         <i slot="icon" class="iconfont iconwenda"></i>
         <span class="text">问答</span>
       </van-tabbar-item>
-      <van-tabbar-item  to="/video">
+      <van-tabbar-item icon="home-o" to="/video">
         <i slot="icon" class="iconfont iconshipin"></i>
         <span class="text">视频</span>
       </van-tabbar-item>
-      <van-tabbar-item to="/my">
+      <van-tabbar-item icon="home-o" to="/my">
         <i slot="icon" class="iconfont iconwode"></i>
-        <span class="text">{{ $store.state.user ? '我的' : '未登录'}}</span>
+        <span class="text">{{ $store.state.user ? '我的' : '未登录' }}</span>
       </van-tabbar-item>
     </van-tabbar>
-    <!-- /标签导航栏 -->
   </div>
 </template>
 
 <script>
 export default {
-  name: 'LayoutIndex'
+  name: 'LayoutIndex',
+  data() {
+    return {
+      active: 0
+    }
+  }
 }
 </script>
 
-<style lang='less' scoped>
+<style lang="less" scoped>
 .layout-container {
   .layout-tabbar {
     i.iconfont {
