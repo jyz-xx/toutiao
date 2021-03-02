bb110aa15b7521f7994e2aee609f759f2a3622f8
jyz-xx <215774124@qq.com>
2021-03-02 14:57:32 +0800

    06-搜索框

---
+++ src/views/search/index.vue
@@ -0,0 +1,49 @@
+<template>
+  <div class="search-container">
+    <!-- 顶部的搜索栏 -->
+  <form action="/">
+    <van-search
+      v-model="value"
+      show-action
+      placeholder="请输入搜索关键词"
+      background="#3296fa"
+      @search="onSearch"
+      @cancel="onCancel"
+    />
+  </form>
+    <!-- /顶部的搜索栏 -->
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'SearchIndex',
+  components: {},
+  props: {},
+  data () {
+    return {
+      value: ''
+    }
+  },
+  computed: {},
+  watch: {},
+  created () {},
+  mounted () {},
+  methods: {
+    onSearch (val) {
+      console.log(val)
+    },
+    onCancel  () {
+      this.$router.back()
+    }
+  }
+}
+</script>
+
+<style lang='less' scoped>
+  .search-container {
+    .van-search_action {
+      color: #fff;
+    }
+  }
+</style>
